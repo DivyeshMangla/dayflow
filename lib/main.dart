6194258f49fb6e06f70d08af5528e05894c72230
DivyeshMangla <174847645+DivyeshMangla@users.noreply.github.com>
2026-06-01 22:49:21 +0530

    feat: initialize Hive database in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:dayflow/screens/app_shell.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+
   runApp(const MyApp());
 }
 
